3b6f595c7350b52ce34531aeee02f8a8bde0818c
waleed677 <waleedmalik677@gmail.com>
2022-03-30 01:31:52 +0500

    add social media account

--- src/components/SocialMedia/Social.js
+++ src/components/SocialMedia/Social.js
@@ -10,14 +10,14 @@ function Social() {
   return (
     <>
          <div className="flexContainer">
-             <div className="flexItems">
+             {/* <div className="flexItems">
              <a href="https://www.instagram.com/twistedbearsnft/" target="_blank"> <img src={"config/images/instagram.png"}  /></a>
+             </div> */}
+             <div className="flexItems">
+             <a href="https://twitter.com/Meta_T_Warriors" target="_blank"><img src={"config/images/twitter.png"}  /></a>
              </div>
              <div className="flexItems">
-             <a href="https://twitter.com/twistedbears_" target="_blank"><img src={"config/images/twitter.png"}  /></a>
-             </div>
-             <div className="flexItems">
-             <a href="https;//discord.gg/twistedbears" target="_blank"> <img src={"config/images/discord.png"} /></a>
+             <a href="https://discord.gg/UWSbtwHkkb" target="_blank"> <img src={"config/images/discord.png"} /></a>
              </div>
          </div>
     </>
